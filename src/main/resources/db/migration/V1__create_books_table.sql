b8ef709f31ee99c5cc44b7cd5c5b049a710c08c8
Doklea Meci <mdoklea@gmail.com>
2019-01-13 21:07:47 +0100

    Book and Genre migration files

--- src/main/resources/db/migration/V1__create_books_table.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE books (
-  id                UUID       PRIMARY  KEY       NOT NULL,
-  isbn              VARCHAR    UNIQUE             NOT NULL,
-  file_name         VARCHAR                       NOT NULL,
-  image_url_path    VARCHAR                       NOT NULL,
-  title             VARCHAR                       NOT NULL,
-  author            VARCHAR                       NULL
-)
--- src/main/resources/db/migration/V2__create_genres_table.sql
+++
@@ -1,4 +0,0 @@
-CREATE TABLE genres (
-  id         UUID PRIMARY KEY              NOT NULL,
-  category   VARCHAR                       NOT NULL
-  )
